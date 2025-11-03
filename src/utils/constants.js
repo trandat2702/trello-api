6dba153090d93592fe91683ce8bf12922d687ff9
TQD <dat27021003@gmail.com>
2025-11-04 02:50:56 +0700

    update CORS config for dev mode

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
 //Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // 'http://localhost:5173'
+  // //Không cần localhost vì ở file
+  // config/cors.js chúng ta đã luôn luôn cho phép môi trường dev truy cập rồi
 ]
 export const BOARD_TYPES = {
   PUBLIC: 'public',
